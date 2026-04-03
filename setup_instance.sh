219e930607dadbdc8d2c1367d03863307505d04c
pravinyadav20 <pravinbaijnathyadav@gmail.com>
2026-04-03 21:33:13 +0530

    Update setup_instance.sh

--- setup_instance.sh
+++ setup_instance.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 # Update & install Docker
-sudo apt-get update -y
-sudo apt-get install -y docker.io
-sudo systemctl enable docker
-sudo systemctl start docker
+apt-get update -y
+apt-get install -y docker.io
+systemctl start docker
+systemctl enable docker
 
-# Optional: install Nginx inside Docker container
-sudo docker pull nginx:latest
-sudo docker run -d -p 80:80 --name terraform-nginx nginx:latest
+# Login to Docker Hub
+docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
+
+# Pull and run your image
+docker pull pravinyadav20/terraform-app:latest
+docker run -d -p 80:80 pravinyadav20/terraform-app:latest
+
+# Start NGINX if needed
+systemctl start nginx
+systemctl enable nginx
